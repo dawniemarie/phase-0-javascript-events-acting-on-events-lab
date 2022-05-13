ee13e53d5d1dcb94566bc8728e7cae417cc3995f
Dawn Olivier <dmarieolivier@gmail.com>
2022-05-12 21:39:43 -0400

    commit tree 99729442ba2b026df6e38ddf4c2b945fe2316cd7

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getElementById("dodger"); //grab the dodger
+dodger.style.backgroundColor="#FF69B4"; //changed color of dodger to pink
+
+//function to move dodger to the left but not exceed left edge of game field
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers,10);
+
+    if (left>0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+//event listener for dodger and left arrow key
+document.addEventListener("keydown",function(e){
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+});
+
+//function to move dodger to the right but not exceed right edge of game field
+function moveDodgerRight(){
+    const newPosition = dodger.style.left.replace("px","");
+    const left = parseInt(newPosition,0);
+
+    if (left<360){
+        dodger.style.left = `${left+1}px`;
+    }
+}
+//event listener for dodger and right arrow key
+document.addEventListener("keydown",function(e){
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
